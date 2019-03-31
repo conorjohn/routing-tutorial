d09df9e0be85e18e7f4a01f7021245e19ec29c45
conorjohn <conorj92@gmail.com>
2019-03-31 20:45:37 +0100

    chore: adding components to be used as pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
